7d1736c9573f547ec84a0e29d1daf368cc6d2eef
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-05-11 19:25:20 +0300

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 def app():
+
     st.title("📊 Dashboard")
 
-    st.info("Dashboard will include analytics later")
+    st.info("Dashboard will be built later (KPIs, charts, revenue)")
