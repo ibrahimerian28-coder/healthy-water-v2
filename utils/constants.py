88dccce6fdeab462ea53a1ef1b1e0cd7fe435375
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-05-11 20:00:55 +0300

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,61 @@
+# =========================
+# AREAS
+# =========================
+
+AREAS = [
+
+    "New Capital",
+    "Capital Gardens",
+    "Madinaty",
+    "El Shorouk",
+    "Badr",
+    "Mostakbal City",
+    "Al Rehab",
+
+    "New Cairo 1",
+    "New Cairo 3",
+    "New Cairo 5",
+
+    "Nasr City",
+    "Heliopolis",
+    "Abbassia",
+    "Downtown",
+
+    "Dokki",
+    "Giza",
+    "Haram",
+
+    "Hadayek Al Ahram",
+
+    "Sheikh Zayed",
+    "6 October",
+
+    "Other"
+]
+
+# =========================
+# STATUS
+# =========================
+
+CUSTOMER_STATUS = [
+    "Active",
+    "Inactive"
+]
+
+# =========================
+# DEVICE TYPES
+# =========================
+
+DEVICE_TYPES = [
+
+    "7 Stages",
+    "7 Stages UV",
+
+    "5 Stages",
+    "5 Stages UV",
+
+    "3 Stages",
+    "3 Stages UV",
+
+    "Other"
+]
