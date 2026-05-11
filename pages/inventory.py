aac6c22950c24bb62c6050b5eab8e658f469c793
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-05-11 19:12:24 +0300

    Update inventory.py

--- pages/inventory.py
+++ pages/inventory.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 def app():
-    st.title("📦 Inventory Page")
-    st.write("هنا هيتم نقل كود العملاء لاحقًا")
+
+    st.title("📦 Inventory")
+
+    gid = st.session_state.SHEETS["Inventory"]
+    url = f"https://docs.google.com/spreadsheets/d/1RGDGJaP_lo2Fp2beLqAQvLulqMk2WDJKqLv2g34-ycc/export?format=csv&gid={gid}"
+
+    df = pd.read_csv(url).fillna("")
+
+    st.dataframe(df)
