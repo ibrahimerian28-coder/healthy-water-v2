641b9ca3e10f1949bd0807ac4dd04e8682b30efc
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-05-11 19:26:52 +0300

    Update expenses.py

--- pages/expenses.py
+++ pages/expenses.py
@@ -1,22 +1,15 @@
 import streamlit as st
-import pandas as pd
+from utils.data_service import load_sheet
 
 def app():
 
     st.title("💵 Expenses")
 
-    try:
-        gid = st.session_state.SHEETS["Expenses"]
+    gid = st.session_state.SHEETS["Expenses"]
+    df = load_sheet(gid)
 
-        url = f"https://docs.google.com/spreadsheets/d/1RGDGJaP_lo2Fp2beLqAQvLulqMk2WDJKqLv2g34-ycc/export?format=csv&gid={gid}"
+    st.subheader("Expenses Table")
+    st.dataframe(df)
 
-        df = pd.read_csv(url).fillna("")
-        df.columns = df.columns.str.strip()
-
-        st.subheader("📊 Expenses Data")
-        st.dataframe(df)
-
-        st.write("عدد السجلات:", len(df))
-
-    except Exception as e:
-        st.error(f"Error loading expenses: {e}")
+    total = len(df)
+    st.write("عدد المصروفات:", total)
