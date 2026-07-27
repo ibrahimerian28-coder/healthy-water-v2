c8bc5058658d8758ac3cd57adb31d4ff31985a8d
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-07-27 22:57:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ section[data-testid="stSidebar"] > div{
 }
 </style>
 """, unsafe_allow_html=True)
-APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzksKYGIuN41Pszdy3Bs_0UQa6kV9XV3lZe41o2_qmcSEAlcADF0TN0qcBhRXCYXjBc1A/exec"
+APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby2ZijzCzTSjUKJ9CtoMXxBv3vX3sq5J0rod3y12XVlHl2vGx_RVAdJaSYIK_1qPnr-wg/exec"
 
 WEB_APP_URL = APP_SCRIPT_URL
 LOGO_PATH = "assets/images/logo.png"
