e8e4305b8ebb319bb8552b45ebd3b86d6c2ce097
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-07-29 13:01:40 +0300

    befor code cleanup

--- modules/customers.py
+++ modules/customers.py
@@ -15,32 +15,6 @@ from utils.constants import (
     DEVICE_TYPES
 )
 
-# =========================
-# HELPERS
-# =========================
-
-def clean_phone(p):
-
-    if pd.isna(p):
-        return ""
-
-    p = str(p).strip()
-
-    if p.lower() in ["nan", "none"]:
-        return ""
-
-    return p
-
-def wa_link(phone):
-
-    phone = clean_phone(phone)
-
-    phone = phone.replace(" ", "")
-
-    if phone.startswith("0"):
-        phone = "2" + phone
-
-    return f"https://wa.me/{phone}"
 
 # =========================
 # APP
