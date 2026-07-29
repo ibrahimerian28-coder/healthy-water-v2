e15b09de8ac07a5166a9ed0e477c028a6eb64bb8
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-07-29 15:10:55 +0300

    Update customer_actions.py

--- modules/components/customer_actions.py
+++ modules/components/customer_actions.py
@@ -1,21 +1,24 @@
 import streamlit as st
-import pandas as pd
 
-from utils.data_service import (
-    update_row,
-    delete_row_by_uuid
-)
+def customer_actions(row, customer_uuid):
 
-from utils.constants import (
-    AREAS,
-    DEVICE_TYPES,
-    CUSTOMER_STATUS
-)
-def customer_actions(
-    row,
-    customer_uuid,
-    AREAS,
-    CUSTOMER_STATUS,
-    DEVICE_TYPES
-):
-    pass
+    col1, col2 = st.columns(2)
+
+    edit_clicked = False
+    delete_clicked = False
+
+    with col1:
+
+        delete_clicked = st.button(
+            "🗑️ Delete",
+            key=f"del_{customer_uuid}"
+        )
+
+    with col2:
+
+        edit_clicked = st.button(
+            "✏️ Edit",
+            key=f"edit_{customer_uuid}"
+        )
+
+    return edit_clicked, delete_clicked
