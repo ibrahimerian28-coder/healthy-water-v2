dcb590e840c74ed96cb5fda0bcbec69420291e73
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-07-29 13:55:41 +0300

    Create customer_search.py

---
+++ modules/components/customer_search.py
@@ -0,0 +1,17 @@
+import pandas as pd
+def search_customers(df, search):
+
+    if not search:
+        return df
+
+    return df[
+        df.astype(str)
+        .apply(
+            lambda x: x.str.contains(
+                search,
+                case=False,
+                na=False
+            )
+        )
+        .any(axis=1)
+    ]
