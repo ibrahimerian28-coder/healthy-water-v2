5b12e69892a027cfee1a8cf330f0aa059390cdc5
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-07-30 16:42:23 +0300

    Update inventory_history_service.py

--- utils/inventory_history_service.py
+++ utils/inventory_history_service.py
@@ -12,16 +12,20 @@ def add_inventory_history(
     technician,
     notes=""
 ):
+
     row = [
-        str(uuid.uuid4()),                              # uuid
-        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),   # date
-        movement,                                       # movement
-        item_name,                                      # item_name
-        quantity,                                       # quantity
-        reference,                                      # reference
-        technician,                                     # technician
-        notes                                           # notes
+        str(uuid.uuid4()),
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        movement,
+        item_name,
+        quantity,
+        reference,
+        technician,
+        notes
     ]
+
+    import streamlit as st
+    st.write("History Row:", row)   # ← أضف هذا السطر
 
     return add_row(
         "Inventory_History",
