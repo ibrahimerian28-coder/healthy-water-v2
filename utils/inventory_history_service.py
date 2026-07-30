60cf17a2246467eedde63a9441daf58443cee38d
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-07-30 16:29:02 +0300

    Update inventory_history_service.py

--- utils/inventory_history_service.py
+++ utils/inventory_history_service.py
@@ -1,29 +1,26 @@
 from datetime import datetime
+import uuid
+
 from utils.data_service import add_row
 
 
 def add_inventory_history(
+    movement,
     item_name,
-    qty,
-    movement_type,
+    quantity,
     reference,
     technician,
-    history_gid
+    notes=""
 ):
-    """
-    movement_type:
-        OUT = خصم
-        IN  = إضافة
-        ADJUST = تعديل
-    """
-
     row = [
-        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),  # الحركة
-        item_name,                                     # الصنف
-        qty,                                           # الكمية
-        movement_type,                                 # نوع الحركة
-        reference,                                     # رقم الزيارة أو السبب
-        technician                                     # الفني
+        str(uuid.uuid4()),                              # uuid
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),   # date
+        movement,                                       # movement
+        item_name,                                      # item_name
+        quantity,                                       # quantity
+        reference,                                      # reference
+        technician,                                     # technician
+        notes                                           # notes
     ]
 
     return add_row(
