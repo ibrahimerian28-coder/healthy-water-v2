33de37789f35bfe2d83a6c7f733dd841b573137b
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-07-30 14:53:16 +0300

    Update parts_manager.py

--- components/parts_manager.py
+++ components/parts_manager.py
@@ -167,6 +167,5 @@ def render_parts_manager(df_inventory):
                 st.rerun()
 
     used_parts.extend(st.session_state.other_parts_list)
-    st.write("RETURN USED PARTS")
-    st.write(used_parts)
+    
     return used_parts
